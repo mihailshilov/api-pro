8ffe84cb908d2f7f924567f952fc85a622a4d514
root <root@api.shilov.pro>
2023-01-06 23:01:04 +0300

    second commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main()  {
-	
+import (
+	"log"
+
+	apipro "github.com/mihailshilov/api-pro"
+	"github.com/mihailshilov/api-pro/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(apipro.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
